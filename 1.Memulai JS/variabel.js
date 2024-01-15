6e375001ba4ac1d1a72f0471bdafcebf4b20686c
jidanasmari <jidanasmari9@gmail.com>
2024-01-15 13:18:46 +0800

    belajar operator dan fungsi

---
+++ 1.Memulai JS/variabel.js
@@ -0,0 +1,19 @@
+//deklarasi awal
+let nama = "zidan";
+let usia = 17;
+const jk = "L";
+
+//cetak niai awal
+console.log(nama);
+console.log(usia);
+console.log(jk);
+
+//perubahan isi variabel
+nama = "zidan";
+usia = 17;
+jk = "L";
+
+//cetak hasil perubahan
+console.log(nama);
+console.log(usia);
+console.log(jk);
---
+++ 2.Operator & fungsi/aritmatika.js
@@ -0,0 +1,38 @@
+let bil1 = 10;
+let bil2 = 3;
+
+//operator pejumlahan
+let jumlah;
+jumlah = bil1 + bil2;
+console.log(jumlah)
+
+//operator pengurangan
+let kurang;
+kurang = bil1 - bil2;
+console.log(kurang)
+
+//operator perkalian
+let kali;
+kali = bil1 * bil2;
+console.log(kali)
+
+//operator pembagian
+let bagi;
+bagi = bil1 / bil2;
+console.log(bagi)
+
+//eksponen (pangkat)
+let pangkat;
+pangkat = bil1 ** bil2;
+console.log(pangkat)
+
+//modulus (sisa hasil bagi)
+let modulus;
+modulus = bil1 % bil2;
+console.log(modulus)
+
+//increment | decrement
+bil1++;
+console.log(bil1);
+bil2--;
+console.log(bil2)
